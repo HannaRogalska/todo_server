c4913fe862410a6dff3102d031c45dafb27382f3
Hanna Rogalska <hanrogalska@gmail.com>
2025-10-20 15:19:15 +0200

    refresh token

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   getProfile,
+  refreshToken
 } from "../controllers/authController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js"
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh-token", refreshToken);
 router.get("/profile", authMiddleware,  getProfile);
 
 export default router;
